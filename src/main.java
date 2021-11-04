735bf3d892575d3589504e91fcce0edde52bd1fb
Brandon Xie <30637265+xiushak@users.noreply.github.com>
2021-11-04 12:17:15 -0400

    updated main method to be more readable

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 import controller.BasicController;
+import controller.MultiBasicController;
+import controller.MultiSimpleController;
 import controller.SimpleController;
+import model.EllipseFishEyeModel;
 import model.FaceRecognitionFishEyeModel;
 import model.FishEyeModel;
 import model.SimpleModel;
@@ -11,9 +14,36 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        //SimpleModel model = new EllipseFishEyeModel();
-        //SimpleModel model = new FishEyeModel();
-        SimpleModel model = new FaceRecognitionFishEyeModel(new FishEyeModel());
+        useEllipseModel();
+        useFisheyeModel();
+        useFaceRecognitionModel();
+        useMultiModel();
+    }
+
+    private static void useEllipseModel() throws IOException {
+        SimpleModel model = new EllipseFishEyeModel();
+        SimpleView view = new BasicView(model);
+        SimpleController controller = new BasicController(model);
+
+        controller.setImage(new File("grid.jpg"));
+        controller.processImage(model.getWidth() / 2, model.getHeight() / 2);
+
+        view.outputImage("ellipseFisheyeGrid.png");
+    }
+
+    private static void useFisheyeModel() throws IOException {
+        SimpleModel model = new FishEyeModel();
+        SimpleView view = new BasicView(model);
+        SimpleController controller = new BasicController(model);
+
+        controller.setImage(new File("grid.jpg"));
+        controller.processImage(model.getWidth() / 2, model.getHeight() / 2);
+
+        view.outputImage("FisheyeGrid.png");
+    }
+
+    private static void useFaceRecognitionModel() throws IOException {
+        SimpleModel model = new FaceRecognitionFishEyeModel();
         SimpleView view = new BasicView(model);
         SimpleController controller = new BasicController(model);
 
@@ -21,5 +51,12 @@ public class main {
         controller.processImage(model.getWidth() / 2, model.getHeight() / 2);
 
         view.outputImage("faceFisheye.png");
+    }
+
+    private static void useMultiModel() throws IOException {
+        MultiSimpleController cont = new MultiBasicController();
+        cont.uploadFolder(new File("face.jpg"));
+        cont.processAll();
+        cont.outputAll();
     }
 }
